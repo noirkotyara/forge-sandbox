1df5185d63265e26ab62b564e057faf618c4b8ae
Diana Maliukh <noirkotyara@gmail.com>
2025-09-15 12:05:10 +0300

    FS-2 Init project + added 2 screens (auth,repos)

---
+++ src/client/main-admin-page/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ForgeReconciler, { Box } from '@forge/react';
+import { Router, Route, Routes } from 'react-router';
+import ReposPage from './repos-page';
+import AuthPage from './auth-page';
+import useRouting from '../../../ui/hooks/useRouting.hook';
+
+const App = () => {
+  const { historyState, navigator } = useRouting();
+
+  if (!navigator || !historyState) {
+    return <Box>Loading...</Box>;
+  }
+
+  return (
+    <Box>
+      <Router
+        navigator={navigator}
+        navigationType={historyState.action as never}
+        location={historyState.location}
+      >
+        <Routes>
+          <Route path="/auth" element={<AuthPage />}></Route>
+          <Route path="/repos" element={<ReposPage />}></Route>
+        </Routes>
+      </Router>
+    </Box>
+  );
+};
+ForgeReconciler.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
---
+++ ui/common/Link.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useNavigate } from 'react-router';
+
+export default function Link({ to, children }) {
+  const navigate = useNavigate();
+  return (
+    <a
+      href={to}
+      onClick={(event) => {
+        event.preventDefault();
+        navigate(to);
+      }}
+    >
+      {children}
+    </a>
+  );
+}
